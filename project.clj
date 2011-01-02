7c9b6ba1e5f8b13691b2ea725d1121f87d350e3d
Ari Lerner <arilerner@mac.com>
2011-01-01 22:27:33 -0800

    Updated version for clojars

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject letsparty "1.0.0-SNAPSHOT"
+(defproject letsparty "1.0.1-SNAPSHOT"
   :description "Event system"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
