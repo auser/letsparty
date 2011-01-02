2765552a5feb090f22e8e7b5fdbd4bde2e8580db
Ari Lerner <arilerner@mac.com>
2011-01-01 22:25:19 -0800

    Updated project.clj to remove unused dependencies

--- project.clj
+++ project.clj
@@ -2,6 +2,5 @@
   :description "Event system"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [pattern-match "1.0.0"]
                  [com.hazelcast/hazelcast "1.9"]
-                 ])
+                ])
